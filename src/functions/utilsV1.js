634722558b203572f17512b97cb501c3a2e9cb3c
Horziox <horziox.dev@gmail.com>
2022-03-02 20:31:36 +0100

    cleanup

--- src/functions/utilsV1.js
+++ src/functions/utilsV1.js
@@ -1,35 +1,7 @@
 const { writeFileSync } = require('fs');
 const { join } = require('path');
 
-const Categorie = require('../models/v1/categorie');
 const { prodBaseURL } = require('../../config.json');
-
-function parseCat(id) {
-    return new Promise((res) => {
-        if(id == null) {
-            return res(null);
-        }
-        const ids = id.toString(2).split("").map(b => parseInt(b)).reverse();
-        let index = 1;
-        let result = [];
-
-        Categorie.findAll()
-        .then(categories => {
-            const data = JSON.parse(JSON.stringify(categories));
-
-            for(const bit of ids) {
-                if(bit === 1) {
-                    const cat = data.find(c => c.id === index);
-                    delete cat.createdAt;
-                    delete cat.updatedAt;
-                    result.push(cat);
-                }
-                index += index;
-            }
-            res(result);
-        });
-    });
-}
 
 function writeProductImage(imageData, imageName) {
     const bitmap = Buffer.from(imageData.replace('data:image/png;base64,', ''), 'base64');
@@ -39,7 +11,6 @@ function writeProductImage(imageData, imageName) {
 function parseProduct(data) {
     return new Promise(async (res) => {
         const d = JSON.parse(JSON.stringify(data));
-        d.type = await parseCat(d.type);
         if(d.image !== null) d.image = `${prodBaseURL}/img/products/${d.image}`;
 
         res(d);
@@ -47,7 +18,6 @@ function parseProduct(data) {
 }
 
 module.exports = {
-    parseCat,
     writeProductImage,
     parseProduct
 }
