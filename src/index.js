c284cf117a83ac4c83a53957fd1795a389df69fd
Horziox <horziox.dev@gmail.com>
2022-03-03 17:41:28 +0100

    use cors policy

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+const cors = require('cors');
 const app = require('./app');
+
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
+
 app.listen('5000', () => console.log("Server Running"));
