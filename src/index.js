a866d4da0d90469634d3c2800c807f4b532aeeb0
Horziox <horziox.dev@gmail.com>
2022-03-09 15:42:34 +0100

    test cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const cors = require('cors');
 const app = require('./app');
 
-app.use(cors({
-    origin: '*',
-    credentials: true
-}));
+app.use(
+    cors({
+        origin: 'http://localhost:4200',
+        methods: "GET,POST,PUT,DELETE",
+        allowedHeaders: [ "Authorization", "Content-Type" ]
+    })
+);
 
 app.listen('5000', () => console.log("Server Running"));
